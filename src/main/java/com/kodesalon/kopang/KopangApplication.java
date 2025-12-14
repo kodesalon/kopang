6f82ec3fd2bd3107c57b802ecd26a0512af4fef5
chanwoo <chocw0402@gmail.com>
2025-12-15 00:51:07 +0900

    initial commit

---
+++ src/main/java/com/kodesalon/kopang/KopangApplication.java
@@ -0,0 +1,13 @@
+package com.kodesalon.kopang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KopangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KopangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodesalon/kopang/KopangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodesalon.kopang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KopangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
