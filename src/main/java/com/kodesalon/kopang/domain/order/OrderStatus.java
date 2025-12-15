27777d8eea7062f4d46bff722014b19666239d89
chanwoo <chocw0402@gmail.com>
2025-12-15 20:51:50 +0900

    feature : 주문 상태 enum 클래스 생성

--- src/main/java/com/kodesalon/kopang/domain/order/OrderStatus.java
+++ src/main/java/com/kodesalon/kopang/domain/order/OrderStatus.java
@@ -1,4 +1,16 @@
 package com.kodesalon.kopang.domain.order;
 
 public enum OrderStatus {
+	PENDING,
+	CANCELLED,
+	PAID,
+	;
+
+	public boolean isCanceled() {
+		return this == CANCELLED;
+	}
+
+	public boolean isPaid() {
+		return this == PAID;
+	}
 }
