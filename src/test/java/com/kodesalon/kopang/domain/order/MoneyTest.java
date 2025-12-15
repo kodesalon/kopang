7855c9a10f41ada394295b862fdfeab786a94d96
chanwoo <chocw0402@gmail.com>
2025-12-15 20:54:19 +0900

    test : Money 객체 테스트

--- src/test/java/com/kodesalon/kopang/domain/order/MoneyTest.java
+++ src/test/java/com/kodesalon/kopang/domain/order/MoneyTest.java
@@ -1,4 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.kodesalon.kopang.domain.order;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class MoneyTest {
-  
+
+	@DisplayName("기존 금액이 0원일 때 합산 결과를 검증한다")
+	@Test
+	void plus() {
+		Money zero = Money.ZERO;
+		Money expected = new Money(1000L);
+
+		Money actual = zero.plus(expected);
+
+		assertEquals(expected, actual);
+	}
 }
