aaec6495394a0d1c9b933a9dcc7eacb7e70616aa
chanwoo <chocw0402@gmail.com>
2025-12-16 18:14:54 +0900

    feature : stock jpa entity 생성

--- src/main/java/com/kodesalon/kopang/storage/stock/StockJpaEntity.java
+++ src/main/java/com/kodesalon/kopang/storage/stock/StockJpaEntity.java
@@ -1,4 +1,49 @@
 package com.kodesalon.kopang.storage.stock;
 
+import com.kodesalon.kopang.domain.stock.Stock;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "stock")
 public class StockJpaEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long no;
+
+	@Column(nullable = false)
+	private Long productNo;
+
+	@Column(nullable = false)
+	private Integer quantity;
+
+	protected StockJpaEntity() {
+	}
+
+	public StockJpaEntity(Long productNo, Integer quantity) {
+		this.productNo = productNo;
+		this.quantity = quantity;
+	}
+
+	public Stock toDomain() {
+		return new Stock(no, productNo, quantity);
+	}
+
+	public Long getNo() {
+		return no;
+	}
+
+	public Long getProductNo() {
+		return productNo;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
 }
