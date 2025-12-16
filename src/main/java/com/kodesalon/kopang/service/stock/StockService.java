339be3c4e3b60905e26d38b6c79b904f9a58fa11
chanwoo <chocw0402@gmail.com>
2025-12-16 18:20:55 +0900

    feature : 재고 서비스 클래스 생성

--- src/main/java/com/kodesalon/kopang/service/stock/StockService.java
+++ src/main/java/com/kodesalon/kopang/service/stock/StockService.java
@@ -1,4 +1,27 @@
 package com.kodesalon.kopang.service.stock;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kodesalon.kopang.service.exception.NotFoundException;
+import com.kodesalon.kopang.domain.stock.Stock;
+import com.kodesalon.kopang.domain.stock.StockRepository;
+
+@Service
 public class StockService {
+
+	private final StockRepository stockRepository;
+
+	public StockService(StockRepository stockRepository) {
+		this.stockRepository = stockRepository;
+	}
+
+	@Transactional
+	public Stock decrease(Long productNo) {
+		Stock stock = stockRepository.findByProductNo(productNo)
+			.orElseThrow(() -> NotFoundException.stock(productNo));
+		Stock decreased = stock.decrease(1);
+		stockRepository.updateStock(decreased);
+		return decreased;
+	}
 }
