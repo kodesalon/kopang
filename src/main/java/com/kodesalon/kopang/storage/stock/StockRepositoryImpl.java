693ecb349667c798719257f52280815a085c5be8
chanwoo <chocw0402@gmail.com>
2025-12-16 18:16:24 +0900

    feature : stock repository 인터페이스 구현

--- src/main/java/com/kodesalon/kopang/storage/stock/StockRepositoryImpl.java
+++ src/main/java/com/kodesalon/kopang/storage/stock/StockRepositoryImpl.java
@@ -1,4 +1,29 @@
 package com.kodesalon.kopang.storage.stock;
 
-public class StockRepositoryImpl {
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.kodesalon.kopang.domain.stock.Stock;
+import com.kodesalon.kopang.domain.stock.StockRepository;
+
+@Repository
+public class StockRepositoryImpl implements StockRepository {
+
+	private final StockJpaRepository stockJpaRepository;
+
+	public StockRepositoryImpl(StockJpaRepository stockJpaRepository) {
+		this.stockJpaRepository = stockJpaRepository;
+	}
+
+	@Override
+	public Optional<Stock> findByProductNo(Long productNo) {
+		StockJpaEntity jpaEntity = stockJpaRepository.findByProductNoWithLock(productNo);
+		return Optional.ofNullable(jpaEntity).map(StockJpaEntity::toDomain);
+	}
+
+	@Override
+	public void updateStock(Stock stock) {
+		stockJpaRepository.updateStock(stock.getProductNo(), stock.getQuantity());
+	}
 }
