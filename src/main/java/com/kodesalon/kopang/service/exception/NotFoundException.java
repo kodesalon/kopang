7857ceb12cc6de11fd19d4644fec31a0355e9008
chanwoo <chocw0402@gmail.com>
2025-12-17 15:19:07 +0900

    add : 상품 not found exception 메서드 추가

--- src/main/java/com/kodesalon/kopang/service/exception/NotFoundException.java
+++ src/main/java/com/kodesalon/kopang/service/exception/NotFoundException.java
@@ -9,4 +9,8 @@ public class NotFoundException extends RuntimeException {
 	public static NotFoundException stock(Long productNo) {
 		return new NotFoundException(String.format("상품 %s 에 대한 재고를 찾을 수 없습니다", productNo));
 	}
+
+	public static NotFoundException product(Long productNo) {
+		return new NotFoundException(String.format("상품 %s 를 찾을 수 없습니다", productNo));
+	}
 }
