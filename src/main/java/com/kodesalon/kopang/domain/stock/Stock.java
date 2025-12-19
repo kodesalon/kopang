48900f5b5ae7a89a39880531a768a2c168d09b52
조찬우 <58971262+oownahcohc@users.noreply.github.com>
2025-12-20 01:50:04 +0900

    Feat/#4/stock product domain : 재고, 상품 도메인 설계 (#5)
    * feature : 상품 도메인 엔티티 생성
    
    * feature : 재고 도메인 엔티티 생성
    
    * test : 재고 도메인 test

---
+++ src/main/java/com/kodesalon/kopang/domain/stock/Stock.java
@@ -0,0 +1,35 @@
+package com.kodesalon.kopang.domain.stock;
+
+public class Stock {
+
+	private static final Integer ZERO = 0;
+
+	private final Long no;
+	private final Long productNo;
+	private final Integer quantity;
+
+	public Stock(Long no, Long productNo, Integer quantity) {
+		this.no = no;
+		this.productNo = productNo;
+		this.quantity = quantity;
+	}
+
+	public Stock decrease(Integer count) {
+		if (quantity.equals(ZERO)) {
+			throw new IllegalStateException("남아있는 재고 수량이 없습니다");
+		}
+		int remainQuantity = quantity - count;
+		if (remainQuantity < ZERO) {
+			throw new IllegalStateException("재고 수량은 0보다 작을 수 없습니다");
+		}
+		return new Stock(no, productNo, remainQuantity);
+	}
+
+	public Stock increase(Integer count) {
+		return new Stock(no, productNo, quantity + count);
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+}
