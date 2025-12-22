8334e7d3289145fff54f4c2983a9d523be92d9d0
chanwoo <chocw0402@gmail.com>
2025-12-23 01:50:28 +0900

    hotfix : 비관락 적용 테스트

--- src/main/java/com/kodesalon/kopang/storage/stock/StockJpaRepository.java
+++ src/main/java/com/kodesalon/kopang/storage/stock/StockJpaRepository.java
@@ -1,11 +1,15 @@
 package com.kodesalon.kopang.storage.stock;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import jakarta.persistence.LockModeType;
+
 public interface StockJpaRepository extends JpaRepository<StockJpaEntity, Long> {
 
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("SELECT s FROM StockJpaEntity s WHERE s.productNo = :productNo")
 	StockJpaEntity findByProductNoWithLock(Long productNo);
 
