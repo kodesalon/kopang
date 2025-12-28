f260d5fd2a52a5146e7d301c0793d7cf5ac916f1
chanwoo <chocw0402@gmail.com>
2025-12-29 01:00:18 +0900

    fix : redis 반환 type Long -> Integer 타입 캐스팅 오류 수정

--- src/main/java/com/kodesalon/kopang/storage/stock/RedisStockReservationRepositoryImpl.java
+++ src/main/java/com/kodesalon/kopang/storage/stock/RedisStockReservationRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.kodesalon.kopang.storage.stock;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -16,22 +17,25 @@ public class RedisStockReservationRepositoryImpl implements StockReservationRepo
 	private static final String PRODUCT_STOCK_KEY_FORMAT = "product:%d:stock";
 
 	private final RedisTemplate<String, String> redisTemplate;
-	private final DefaultRedisScript<Integer> decreaseStockScript;
+	private final DefaultRedisScript<Long> decreaseStockScript;
 
 	public RedisStockReservationRepositoryImpl(RedisTemplate<String, String> redisTemplate) {
 		this.redisTemplate = redisTemplate;
 		this.decreaseStockScript = new DefaultRedisScript<>();
 		this.decreaseStockScript.setLocation(new ClassPathResource("redis/decrease_stock.lua"));
-		this.decreaseStockScript.setResultType(Integer.class);
+		this.decreaseStockScript.setResultType(Long.class);
 	}
 
 	@Override
 	public Stock decreaseStock(Long productNo, Integer count) {
-		Integer remainQuantity = redisTemplate.execute(
+		Long result = redisTemplate.execute(
 			decreaseStockScript,
 			List.of(String.format(PRODUCT_STOCK_KEY_FORMAT, productNo)),
 			String.valueOf(count)
 		);
+		Integer remainQuantity = Optional.of(result)
+			.map(Long::intValue)
+			.orElseThrow();
 		return Stock.of(productNo, remainQuantity);
 	}
 
