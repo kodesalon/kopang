f82fed479ee2b7a8b64932fb8b33945c440f3eab
chanwoo <chocw0402@gmail.com>
2025-12-29 19:12:01 +0900

    feature : order jpa repository 조회 및 조건부 update 메서드 시그니처 추가

--- src/main/java/com/kodesalon/kopang/storage/order/OrderJpaRepository.java
+++ src/main/java/com/kodesalon/kopang/storage/order/OrderJpaRepository.java
@@ -1,6 +1,28 @@
 package com.kodesalon.kopang.storage.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import com.kodesalon.kopang.domain.order.OrderStatus;
 
 public interface OrderJpaRepository extends JpaRepository<OrderJpaEntity, Long> {
+
+	OrderJpaEntity findByNo(Long orderNo);
+
+	@Modifying
+	@Query("UPDATE OrderJpaEntity o SET o.status = :status WHERE o.no = :orderNo")
+	void updateOrder(Long orderNo, OrderStatus status);
+
+	@Modifying
+	@Query("UPDATE OrderJpaEntity o SET o.status = :status WHERE o.no = :orderNo AND o.status = 'PENDING'")
+	int updateStatusToInProgress(Long orderNo, OrderStatus status);
+
+	@Modifying
+	@Query("UPDATE OrderJpaEntity o SET o.status = :status WHERE o.no = :orderNo AND o.status = 'PAYMENT_IN_PROGRESS'")
+	int updateStatusToPending(Long orderNo, OrderStatus status);
+
+	@Modifying
+	@Query("UPDATE OrderJpaEntity o SET o.status = :status WHERE o.no = :orderNo AND o.status <> 'CANCELLED'")
+	int updateStatusCancel(Long orderNo, OrderStatus status);
 }
