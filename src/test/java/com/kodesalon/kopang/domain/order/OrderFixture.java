961b5e7a8e601faecf3e16aa79276727449d2bc0
chanwoo <chocw0402@gmail.com>
2025-12-30 20:17:05 +0900

    refactor : orderedAt 필드 추가

--- src/test/java/com/kodesalon/kopang/domain/order/OrderFixture.java
+++ src/test/java/com/kodesalon/kopang/domain/order/OrderFixture.java
@@ -1,12 +1,13 @@
 package com.kodesalon.kopang.domain.order;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class OrderFixture {
 
 	public static final OrderProduct FIRST_ORDER_PRODUCT = OrderProduct.of(1L, 1L, 1, BigDecimal.valueOf(1000));
-	public static final Order PENDING_ORDER = Order.of(1L, 1L, OrderStatus.PENDING, List.of(FIRST_ORDER_PRODUCT));
+	public static final Order PENDING_ORDER = Order.of(1L, 1L, OrderStatus.PENDING, List.of(FIRST_ORDER_PRODUCT), LocalDateTime.now());
 	public static final Order PAID_ORDER = PENDING_ORDER.pay();
 	public static final Order CANCELLED_ORDER = PENDING_ORDER.cancel();
 }
