7062f43c800988b5187a32cdf78b9fe82bb9d66d
chanwoo <chocw0402@gmail.com>
2026-01-08 16:03:03 +0900

    refactor : OrderJpaRepositoryImpl 조건부 update 메서드 제거

--- src/main/java/com/kodesalon/kopang/storage/order/OrderRepositoryImpl.java
+++ src/main/java/com/kodesalon/kopang/storage/order/OrderRepositoryImpl.java
@@ -36,21 +36,6 @@ public class OrderRepositoryImpl implements OrderRepository {
 	}
 
 	@Override
-	public int updateStatusToInProgress(Order order) {
-		return orderJpaRepository.updateStatusToInProgress(order.getNo(), order.getStatus());
-	}
-
-	@Override
-	public int updateStatusToPending(Order order) {
-		return orderJpaRepository.updateStatusToPending(order.getNo(), order.getStatus());
-	}
-
-	@Override
-	public int updateStatusCancel(Order order) {
-		return orderJpaRepository.updateStatusCancel(order.getNo(), order.getStatus());
-	}
-
-	@Override
 	public List<Order> findExpiredOrders(LocalDateTime cutoffTime) {
 		return orderJpaRepository
 			.findExpiredOrders(List.of(OrderStatus.PENDING, OrderStatus.PAYMENT_IN_PROGRESS), cutoffTime)
