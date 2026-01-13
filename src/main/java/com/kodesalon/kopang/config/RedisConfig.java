0b567939e8539398b8d967e708af351e34d481ae
chanwoo <chocw0402@gmail.com>
2026-01-14 01:16:49 +0900

    feature : RedisConfig cacheManager bean 등록

--- src/main/java/com/kodesalon/kopang/config/RedisConfig.java
+++ src/main/java/com/kodesalon/kopang/config/RedisConfig.java
@@ -1,15 +1,29 @@
 package com.kodesalon.kopang.config;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 
 @Configuration
 public class RedisConfig {
+
+	public static final String CACHE_PRODUCT_WAREHOUSES = "product:warehouses";
 
 	private final String host;
 	private final int port;
@@ -36,5 +50,26 @@ public class RedisConfig {
 		redisTemplate.setValueSerializer(new StringRedisSerializer());
 
 		return redisTemplate;
+	}
+
+	@Bean
+	public CacheManager cacheManager() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
+			.allowIfSubType(Object.class)
+			.build();
+		objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+
+		RedisCacheConfiguration warehouseConfig = RedisCacheConfiguration.defaultCacheConfig()
+			.disableCachingNullValues()
+			.entryTtl(Duration.ofHours(1))
+			.serializeKeysWith(RedisSerializationContext.SerializationPair
+				.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(RedisSerializationContext.SerializationPair
+				.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)));
+
+		return RedisCacheManager.builder(redisConnectionFactory())
+			.cacheDefaults(warehouseConfig)
+			.build();
 	}
 }
