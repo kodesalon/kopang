f6b49de5b230c1a040501d4a58b86a0e8a58d380
chanwoo <chocw0402@gmail.com>
2026-01-13 20:00:00 +0900

    feature : Warehouse 도메인 엔티티 클래스

---
+++ src/main/java/com/kodesalon/kopang/domain/warehouse/Warehouse.java
@@ -0,0 +1,18 @@
+package com.kodesalon.kopang.domain.warehouse;
+
+import com.kodesalon.kopang.domain.Address;
+
+public class Warehouse {
+
+	private final Long no;
+	private final String name;
+	private final WarehouseRegion region;
+	private final Address address;
+
+	public Warehouse(Long no, String name, WarehouseRegion region, Address address) {
+		this.no = no;
+		this.name = name;
+		this.region = region;
+		this.address = address;
+	}
+}
