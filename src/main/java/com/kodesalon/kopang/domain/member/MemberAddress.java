a143132bd28b9e1131b7cab8c8d0f911dba88696
chanwoo <chocw0402@gmail.com>
2026-01-14 17:32:51 +0900

    feature : MemberAddress 도메인 엔티티 클래스

---
+++ src/main/java/com/kodesalon/kopang/domain/member/MemberAddress.java
@@ -0,0 +1,18 @@
+package com.kodesalon.kopang.domain.member;
+
+import com.kodesalon.kopang.domain.Address;
+
+public class MemberAddress {
+
+	private final Long no;
+	private final Long memberNo;
+	private final String alias;
+	private final Address address;
+
+	public MemberAddress(Long no, Long memberNo, String alias, Address address) {
+		this.no = no;
+		this.memberNo = memberNo;
+		this.alias = alias;
+		this.address = address;
+	}
+}
