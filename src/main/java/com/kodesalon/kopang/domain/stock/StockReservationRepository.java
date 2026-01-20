96797283986d47945d240e47a37a2fd2f3640a0a
chanwoo <chocw0402@gmail.com>
2026-01-20 19:55:44 +0900

    refactor : StockResrvationRepository 메서드 시그니처 변경 - Long warehouseNo 추가

--- src/main/java/com/kodesalon/kopang/domain/stock/StockReservationRepository.java
+++ src/main/java/com/kodesalon/kopang/domain/stock/StockReservationRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 public interface StockReservationRepository {
 
-	Optional<StockQuantity> decreaseStock(String warehouseName, Long productNo, Integer count);
+	Optional<StockQuantity> decreaseStock(Long warehouseNo, Long productNo, Integer count);
 
-	void increaseStock(Long productNo, Integer count);
+	void increaseStock(Long warehouseNo, Long productNo, Integer count);
 }
