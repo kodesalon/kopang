cae9c306e78b0dbd7d9a662cd753a74b56778cca
chanwoo <chocw0402@gmail.com>
2026-01-30 01:42:10 +0900

    fix : redis serialization deserialization 문제 record 로 해결 시도

--- src/main/java/com/kodesalon/kopang/domain/Coordinate.java
+++ src/main/java/com/kodesalon/kopang/domain/Coordinate.java
@@ -2,16 +2,10 @@ package com.kodesalon.kopang.domain;
 
 import java.awt.geom.Point2D;
 
-public class Coordinate {
-
-	private final Double latitude;
-	private final Double longitude;
-
-	public Coordinate(Double latitude, Double longitude) {
-		this.latitude = latitude;
-		this.longitude = longitude;
-	}
-
+public record Coordinate(
+	Double latitude,
+	Double longitude
+) {
 	public Double calculateDistance(Coordinate other) {
 		return new Point2D.Double(latitude, longitude)
 			.distance(other.latitude, other.longitude);
